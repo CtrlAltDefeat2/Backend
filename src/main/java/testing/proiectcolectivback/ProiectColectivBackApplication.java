139cfa0deea5a1c1a2a491f3ef02684a7360c484
Horaţiu <42303594+H0R4T1U@users.noreply.github.com>
2025-10-28 20:48:28 +0200

    Initial push

---
+++ src/main/java/testing/proiectcolectivback/ProiectColectivBackApplication.java
@@ -0,0 +1,13 @@
+package testing.proiectcolectivback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectColectivBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProiectColectivBackApplication.class, args);
+    }
+
+}
